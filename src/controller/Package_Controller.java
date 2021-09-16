d98a8fdcc4ac58a7451a860cf63086a9cd972d55
Rajith Sanjaya <79979236+LussyPussy@users.noreply.github.com>
2021-09-16 21:02:46 +0530

    update

--- src/controller/Package_Controller.java
+++ src/controller/Package_Controller.java
@@ -1,4 +1,55 @@
 package controller;
 
+import java.sql.SQLException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Package_Controller {
+    @FXML
+    private TextField lbl_packageName;
+
+    @FXML
+    private TextField lbl_packagePrice;
+
+    @FXML
+    private TextField lbl_packageDescription;
+
+    @FXML
+    private TextField lbl_packageNo;
+
+    @FXML
+    private ComboBox<?> cmb_packageNo;
+    
+      @FXML
+    void save_package(MouseEvent event) {
+        
+        Item item1 =new Item(
+                txtItemCode.getText(),
+                txtDescription.getText(),
+                txtPackSize.getText(),
+                Double.parseDouble(txtUnitPrice.getText()),
+                Integer.parseInt(txtQTYonHand.getText())
+        );
+
+
+
+        if(saveItem(item1))
+            new Alert(Alert.AlertType.CONFIRMATION, "Saved..").show();
+        else
+            new Alert(Alert.AlertType.WARNING, "Try Again..").show();
+
+
+    }
+
+    public void clearData(ActionEvent actionEvent) {
+    }
+    boolean saveItem(Item item) throws SQLException, ClassNotFoundException {
+        return new ItemController().saveItem(item);
+
+    }
+
 }
---
+++ src/model/Package.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Package {
+   String packageId;
+   String packageDetails;
+   double packagePrice;
+}
