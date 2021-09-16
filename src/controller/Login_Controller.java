a148dc255311e193b24f02979b37160100b69ff9
Rajith Sanjaya <79979236+LussyPussy@users.noreply.github.com>
2021-09-16 16:16:08 +0530

    Update

--- src/controller/Login_Controller.java
+++ src/controller/Login_Controller.java
@@ -1,43 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author ishar
- */
-public class Login_Controller implements Initializable {
-    @FXML
-    void open_Settings(MouseEvent event) throws IOException {
-        URL resource = getClass().getResource("../view/setting");
+import java.io.IOException;
+import java.net.URL;
+
+public class Login_Controller {
+
+    public void open_Settings(MouseEvent mouseEvent) throws IOException {
+        URL resource = getClass().getResource("../view/settings.fxml");
         Parent load = FXMLLoader.load(resource);
         Scene scene = new Scene(load);
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.show();
-    }    
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }
-    
 }
