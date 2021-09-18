8b34fe839581c8544704affcceae24c19f014c4c
Rajith Sanjaya <79979236+LussyPussy@users.noreply.github.com>
2021-09-18 13:05:12 +0530

    Update

--- src/controller/Setting_Controller.java
+++ src/controller/Setting_Controller.java
@@ -5,23 +5,32 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author ishar
- */
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class Setting_Controller implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    @FXML
+    void open_manageFilm(MouseEvent event) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/manageFilm.fxml"));
+        Scene scene = new Scene(load);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
